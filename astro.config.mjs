cb58bd7c81955fe7592162fe78b68a9a4c23cb33
Syed Tajir Hasnain <tajircuet@gmail.com>
2025-08-03 13:50:00 +0600

    feat(framework): expressive-code integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,31 @@ import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 
+import expressiveCode from "astro-expressive-code";
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://tajirhasnain.com", // Public URL without port
     base: "/",
-    integrations: [vue(), sitemap(), mdx()],
+    integrations: [vue(), sitemap(), expressiveCode({
+        plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+        themes: ['one-dark-pro', 'solarized-light'],
+        shiki: {}
+    }), mdx()],
     i18n: {
         locales: ["bn", "en"],
         defaultLocale: "en",
     },
     markdown: {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-            themes: {
-                light: "github-dark",
-                dark: "github-dark",
-            },
-            wrap: true,
-        },
+        // syntaxHighlight: "shiki",
+        // shikiConfig: {
+        //     themes: {
+        //         light: "github-dark",
+        //         dark: "github-dark",
+        //     },
+        //     wrap: true,
+        // },
     },
 });
-
